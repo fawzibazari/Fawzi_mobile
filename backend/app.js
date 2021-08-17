e88e822b78a6743d21700bfdf3137a7606e613a7
fawzi bazari <fawzibazari@MacBook-Air-de-fawzi.local>
2021-08-18 00:36:09 +0200

    insertion vers la bdd fonctionnel

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,17 @@ const api = process.env.API_URL
 app.use(express.json());
 app.use(morgan('tiny'));
 
+const productSchema = mongoose.Schema ({
+    name: String,
+    image: String,
+    countInStock: {
+        type: Number,
+        required: true
+    }
+})
+
+const Product = mongoose.model('Product', productSchema)
+
 app.get(`${api}/products`, (req, res) =>{
     const product = {
         id: 1,
@@ -23,9 +34,22 @@ app.get(`${api}/products`, (req, res) =>{
 })
 
 app.post(`${api}/products`, (req, res) =>{
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock,
+
+    });
+
+    product.save().then((createdProduct => {
+        res.status(201).json(createdProduct)
+    })).catch((err)=>{
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+
+    })
 }) 
 
 //connection BDD
