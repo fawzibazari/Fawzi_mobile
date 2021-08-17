733fd38ea04f1b63b8686cdc77519bc775cd7842
fawzi bazari <fawzibazari@MacBook-Air-de-fawzi.local>
2021-08-17 23:26:21 +0200

    premier commit

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+require('dotenv/config');
+const morgan = require('morgan');
+
+
+
+const api = process.env.API_URL
+
+//middleware NARUTO 
+app.use(express.json());
+app.use(morgan('tiny'));
+
+app.get(`${api}/products`, (req, res) =>{
+    const product = {
+        id: 1,
+        name: "hair dresser",
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Argentina_celebrando_copa_%28cropped%29.jpg"
+    }
+
+    res.send(product);
+})
+
+app.post(`${api}/products`, (req, res) =>{
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
+}) 
+app.listen(3000, ()=>{
+    console.log('server de naruto http://localhost:3000');
+})
