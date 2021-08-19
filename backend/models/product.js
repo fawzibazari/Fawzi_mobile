3507c3fb1a852e19a22c73b41c64787a4585c033
fawzi bazari <fawzibazari@MacBook-Air-de-fawzi.local>
2021-08-19 23:09:05 +0200

    insertion des produits fonctionnel avec le ge de tout les produit

--- backend/models/product.js
+++ backend/models/product.js
@@ -44,6 +44,10 @@ const productSchema = mongoose.Schema ({
         type: Number,
         default: 0,
     },
+    numReviews: {
+        type: Number,
+        default: 0,
+    },
     isFeatured: {
         type: Boolean,
         default: false,
--- backend/routers/products.js
+++ backend/routers/products.js
@@ -1,8 +1,9 @@
 const {Product} = require('../models/product');
 const express = require('express');
+const { Category } = require('../models/category');
 const router = express.Router();
 
-router.get(`/`, async (req, res) =>{
+router.get(`/`, async (req, res) =>{  //mon get de tout les produit 
     const productList = await Product.find();
 
     if(!productList) {
@@ -11,21 +12,39 @@ router.get(`/`, async (req, res) =>{
     res.send(productList);
 })
 
-router.post(`/`, (req, res) =>{
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
+router.get(`/:id`, async (req, res) =>{  //mon get pour un seul produit 
+    const productList = await Product.find();
 
-    product.save().then((createdProduct=> {
-        res.status(201).json(createdProduct)
-    })).catch((err)=>{
-        res.status(500).json({
-            error: err,
-            success: false
-        })
+    if(!productList) {
+        res.status(500).json({success: false})
+    } 
+    res.send(productList);
+})
+
+router.post(`/`, async (req, res) =>{
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Category invalid') // la je suis entrain de checker si tout est bon pour valider
+
+    let product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: req.body.image,
+        images: req.body.images,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
     })
+    product = await product.save();
+    
+    if(!product)
+    return res.status(500).send('Produits pas créer')
+
+    res.send(product);
 })
 
 module.exports =router;
