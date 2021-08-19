36c6aaa83c331310cfda9daa2b9f8192ae03a336
fawzi bazari <fawzibazari@MacBook-Air-de-fawzi.local>
2021-08-19 02:54:38 +0200

    CRUD fonctionnel pour les categories

--- backend/routers/categories.js
+++ backend/routers/categories.js
@@ -8,7 +8,16 @@ router.get(`/`, async (req, res) =>{
     if(!categoryList) {
         res.status(500).json({success: false})
     } 
-    res.send(categoryList);
+    res.status(200).send(categoryList);
+})
+
+router.get('/:id', async (req, res) =>{ 
+    const category = await Category.findById(req.params.id);
+    if(!category) {
+        res.status(500).json({message: 'ya pas de categories avec ce id '})
+    } 
+    res.status(200).send(category);
+
 })
 
 router.post(`/`, async (req, res) =>{
@@ -25,6 +34,22 @@ router.post(`/`, async (req, res) =>{
 
     res.send(category);
 })
+
+router.put(`/:id`, async (req, res) =>{
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color,
+        }, {new: true}
+    )
+    if(!category)
+    return res.status(404).send('la category peut pas etre modifier ')
+
+    res.send(category);
+})
+
 
 router.delete('/:id', (req, res) =>{ 
     Category.findByIdAndRemove(req.params.id).then(category =>{
