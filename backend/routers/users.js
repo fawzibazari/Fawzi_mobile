85bd102e14cf6d12d343d2b1c5aadad9b6982084
fawzi bazari <fawzi.bazari2@gmail.com>
2021-08-21 00:39:06 +0200

    ajout du update et le delete des user avec le reglage de plusieur bug

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -45,6 +45,39 @@ router.post(`/`, async (req, res) =>{
     res.send(user);
 })
 
+router.put('/:id',async (req, res)=> {
+
+    const userExist = await User.findById(req.params.id);
+    let newPassword
+    if(req.body.password) {
+        newPassword = bcrypt.hashSync(req.body.password, 10)
+    } else {
+        newPassword = userExist.passwordHash;
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash: newPassword,
+            phone: req.body.phone,
+            isAdmin: req.body.isAdmin,
+            street: req.body.street,
+            apartment: req.body.apartment,
+            zip: req.body.zip,
+            city: req.body.city,
+            country: req.body.country,
+        },
+        { new: true}
+    )
+
+    if(!user)
+    return res.status(400).send('user peut pas etre créer')
+
+    res.send(user);
+})
+
 router.post('/login', async (req,res) => {
     const user = await User.findOne({email: req.body.email})
     const secret = process.env.secret;
@@ -56,7 +89,8 @@ router.post('/login', async (req,res) => {
     if(user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
         const token = jwt.sign(
             {
-                userId: user.id
+                userId: user.id,
+                isAdmin: user.isAdmin
             },
             secret,
             {expiresIn : '1d'}
@@ -90,6 +124,29 @@ router.post(`/register`, async (req, res) =>{
     return res.status(404).send('le user peut pas etre créer')
 
     res.send(user);
+})
+
+router.delete('/:id', (req, res)=>{
+    User.findByIdAndRemove(req.params.id).then(user =>{
+        if(user) {
+            return res.status(200).json({success: true, message: 'le user et supprimer!'})
+        } else {
+            return res.status(404).json({success: false , message: "user pas trouver"})
+        }
+    }).catch(err=>{
+       return res.status(500).json({success: false, error: err}) 
+    })
+})
+
+router.get(`/get/count`, async (req, res) =>{
+    const userCount = await User.countDocuments((count) => count)
+
+    if(!userCount) {
+        res.status(500).json({success: false})
+    } 
+    res.send({
+        userCount: userCount
+    });
 })
 
 module.exports =router;
