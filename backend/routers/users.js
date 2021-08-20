00ecdc143614c93ef82814a9de278610d3d94468
fawzi bazari <fawzi.bazari2@gmail.com>
2021-08-20 13:36:19 +0200

    get des user et get by id fonctionnel

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -5,12 +5,21 @@ const bcrypt = require('bcryptjs');
 
 
 router.get(`/`, async (req, res) =>{
-    const userList = await User.find();
+    const userList = await User.find().select('-passwordHash');
 
     if(!userList) {
         res.status(500).json({success: false})
     } 
     res.send(userList);
+})
+
+router.get('/:id', async (req, res) =>{ 
+    const user = await User.findById(req.params.id).select('-passwordHash');
+    if(!user) {
+        res.status(500).json({message: 'ya pas de user avec ce id '})
+    } 
+    res.status(200).send(user);
+
 })
 
 router.post(`/`, async (req, res) =>{
