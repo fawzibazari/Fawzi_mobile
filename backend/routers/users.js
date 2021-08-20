b68e89896a902bc80f1a7aafe08ac9e07f135a65
fawzi bazari <fawzibazari@MacBook-Air-de-fawzi.local>
2021-08-20 13:13:27 +0200

    ajout des models por le user et creatiodes user avec hashage mdp

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -1,6 +1,8 @@
 const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
 
 router.get(`/`, async (req, res) =>{
     const userList = await User.find();
@@ -9,6 +11,28 @@ router.get(`/`, async (req, res) =>{
         res.status(500).json({success: false})
     } 
     res.send(userList);
+})
+
+router.post(`/`, async (req, res) =>{
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+
+    user = await user.save();
+
+    if(!user)
+    return res.status(404).send('le user peut pas etre créer')
+
+    res.send(user);
 })
 
 module.exports =router;
