9688afd75c7b793febeeebd525be7c95ab99dd78
fawzi bazari <fawzi.bazari2@gmail.com>
2021-10-04 10:23:41 +0200

    début de mon header

--- frontend/Screens/Products/ProductContainer.js
+++ frontend/Screens/Products/ProductContainer.js
@@ -1,32 +1,45 @@
-import React, { useState, useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import {
-  View,
-  Text,
-  StyleSheet,
-  ActivityIndicator,
-  FlatList
+    ActivityIndicator,
+    FlatList, StyleSheet, Text, View
 } from "react-native";
-
-const data = require('../../assets/data/product.json');
 import ProductList from "./ProductList";
 
+import {Container, Header, Icon, Item, Input,} from 'native-base';
 
+const data = require('../../assets/data/product.json');
 
 const ProductContainer = () => {
 
     const [products, setProducts] = useState([]);
+    const [productsFiltered, setProductsFiltered] = useState([]);
+
 
     useEffect (() => {
         setProducts(data);
+        setProductsFiltered(data);
 
         return () => {
             setProducts([])
         }
     }, [])
 return (
+    <Container>
+        <Header searchBar rounded> 
+        <Item>
+     <Icon name="ios-search" />
+     <Input
+       placeholder="rechercher"
+    //    onFocus={openList}
+    //    onChangeText={(text) => searchProduct(text)}
+     />
+     {/* {focus == true ? <Icon onPress={onBlur} name="ios-close" /> : null} */}
+     {/* pas encore fini le back-end du coup je enleve mon focus */}
+   </Item>
+        </Header>
     <View>
         <Text>Container des produits</Text>
-        <View style={{ marginTop: 100,backgroundColor: 'gainsboro' }}>
+        <View style={{ backgroundColor: 'gainsboro' }}>
         <FlatList
         numColumns={2}
         data={products}
@@ -37,6 +50,7 @@ return (
         />
         </View>
     </View>
+    </Container>
 )
 }
 
