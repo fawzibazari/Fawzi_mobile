96226dfe03cf1956ecd7b8abc80757f8cf511f3f
fawzi bazari <fawzi.bazari2@gmail.com>
2021-10-05 10:31:48 +0200

    search

---
+++ frontend/Screens/Products/SearchedProducts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StyleSheet, Dimensions} from 'react-native'
+import { Content, Left, Body, ListItem, Thumbnail, Text } from 'native-base';
+
+const SearchedProduct = (props) => {
+    const { ProductFiltered } = props;
+    return (
+    <Content>
+        {}
+    </Content>
+    )
+}
